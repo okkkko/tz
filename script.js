dc887763f62a0d1a5a9195183170b797f1e55c08
Konopelska Oksana <konopelskaoksana@gmail.com>
2021-02-02 16:04:54 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    arrow: true
+  });
+});
+const input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  initialCountry: "auto",
+  geoIpLookup: function(callback) {
+    $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+      let countryCode = (resp && resp.country) ? resp.country : "us";
+      callback(countryCode);
+    });
+  },
+  utilsScript: "../../build/js/utils.js?1603274336113" 
+});
+var iti = intlTelInput(input)
+var countryData = iti.getSelectedCountryData()
